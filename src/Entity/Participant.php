33a45686c5a5f26ba7cc247bac6cf2572e1cbfce
Kérian MONTES-MORIN <kerianmontes@gmail.com>
2019-05-10 15:33:20 +0200

    update (participant) validators

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ParticipantRepository")
@@ -20,16 +21,23 @@ class Participant
 
     /**
      * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank()
      */
     private $firstname;
 
     /**
      * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank()
      */
     private $lastname;
 
     /**
      * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Participant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class ParticipantType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('email')
+            ->add('email', EmailType::class)
         ;
     }
 
